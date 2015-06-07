9a45758e6ed11fae99f97011a20299e854750703
Kumar Anirudha <anirudhastark@yahoo.com>
2015-06-08 03:44:31 +0530

    trying to fix the runserver issue

--- django_yaba/feeds.py
+++ django_yaba/feeds.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.syndication.feeds import Feed
+# from django.contrib.syndication.feeds import Feed
 from django_yaba.models import Story
 
 class RSSFeed(Feed):
